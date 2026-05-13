d74939ec16b18eeb5ae7c8e3b5d7e19450339deb
vicLopez <vctto8@gmail.com>
2026-05-13 17:10:06 -0600

    feat: add EF Core with SQL Server and initial migration

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagerAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -6,7 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
